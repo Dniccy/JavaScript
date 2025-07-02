8c4623cb24e6f798d764a2111ffce26883adcc1b
Dom <dominicnewbold@outlook.com>
2025-07-02 12:49:58 +0100

    Add files via upload

--- javascript/script.js
+++ javascript/script.js
@@ -1,2 +1,19 @@
-//External JavaScript Example
-alert("Enter at your own risk!");
+//Alert function learning 
+window.onload = function() {
+    alert ("I'm learning JavaScript be kind");
+    console.log("The web page has loaded");
+}
+//Text change learning 
+function changeText() {
+    var heading = document.getElementById("demo")
+    heading.textContent = "See I'm learning!";    
+}
+//Variables learning 
+var myAge = 29;
+
+if (myAge >=18){
+    console.log ("You are and adult.");
+} else {
+    console.log("You are a minor.");
+}
+

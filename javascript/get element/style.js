6fface9c9122631e124cf60ddad0c54102fd451a
Dom <dominicnewbold@outlook.com>
2025-07-16 17:36:35 +0100

    Add files via upload

--- javascript/get element/style.js
+++ javascript/get element/style.js
@@ -12,12 +12,37 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 document.addEventListener("DOMContentLoaded", function () {
-    const highlightParagrahps = document.getElementsByClassName("highlight-paragraph"
+    const highlightParagraphs = document.getElementsByClassName("highlight-paragraph"
     );
 
     // Modify the styles of the slected elements
     for (let i = 0; i < highlightParagrahps.length; i++) {
-        highlightParagrahps[i].style.border = "2px solid yellow";
+        highlightParagrahps[i].style.border = "2px solid purple";
         highlightParagrahps[i].style.borderRadius = "5px";
     }
 });
+// Selecting the section with the class 'highlight-paragraph'
+const section = document.querySelector(".highlight-paragraph");
+
+// Adding an event listener to toggle the highlight on click
+section.addEventListener("click", function () {
+    // Toggling the 'highlight' class on the section
+    this.classList.toggle("highlight");
+});
+
+window.onload = addElement;
+
+function addElement() {
+    // Create a new section element 
+    const newSection = document.createElement("section");
+
+    // Give it some content
+    const newContent = document.createTextNode("Hello there and welcome");
+
+    // Add the text node to the newly created section
+    newSection.appendChild(newContent);
+
+    // Add the newly created element and its content to the DOM
+    const currentSection = document.querySelector("#chapter");
+    document.body.insertBefore(newSection, currentSection);
+}
